0510eef7662b21cddacefaff7c1662538119217a
vigneshrathakrishnan <vigneshrathakrishnan007@gmail.com>
2026-07-13 22:27:47 +0530

    chore: add initial script for process, fs and docker learning

---
+++ learnings/main.js
@@ -0,0 +1,4 @@
+// Program -> Passive executable (main.js)
+// Process -> Running instance of a program (Utilizing RAM and CPU register and computation while running)
+// Virtual Memory -> Isolated memory for Process and it will be scatterd physically at RAM
+const data = await fs.promises.readFile("wallet.db");
